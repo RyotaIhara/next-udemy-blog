1718a58c08f415daec509f78ac1203ed3c7f8504
RyotaIhara <ihara0531@gail.com>
2025-05-13 20:57:06 +0900

    カードを作成

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,7 +1,18 @@
-import React from "react";
+import { getPosts } from "@/lib/post"
+import PostCard from "@/components/post/PostCard"
+import { Post } from "@/types/post"
 
-export default function PostPage() {
+export default async function PostsPage() {
+  const posts = await getPosts()
   return (
-    <div></div>
+    <>
+    <div className="container mx-auto px-4 py-8">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+        {posts.map((post)=>(
+          <PostCard key={post.id} post={post} />
+        ))}
+      </div>
+    </div>
+    </>
   )
 }
